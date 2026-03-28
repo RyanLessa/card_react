665b98a0e1cb473cf6a0d9cbd32b3fa914da01f9
RyanLessa <ryan.lessa@icloud.com>
2026-03-28 15:11:57 -0300

    first commit, application setup

---
+++ cardzin/app/layout.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function WhatIWant() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+          <h1>Meu aplicativoa</h1>
+      </body>
+    </html>
+  )
+}
---
+++ cardzin/app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function RootLayout() {
+  return (
+    <html lang="en">
+      <body>
+        <h1>Meu aplicativo legal</h1>
+      </body>
+    </html>
+  );
+}
