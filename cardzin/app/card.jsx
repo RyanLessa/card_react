f6a99b10b3f70210b937159bc07264955c197dfe
RyanLessa <ryan.lessa@icloud.com>
2026-03-28 15:43:05 -0300

    home page done

--- cardzin/app/card.jsx
+++ cardzin/app/card.jsx
@@ -14,8 +14,8 @@ export default function Card () {
     return (
         <div className="card" id="my-card">
             <Image src="/ryan.jpg" alt="Foto de ryan" width={100} height={100} className='card-image'/>
-            <h1>Nome</h1>
-            <p>Descrição muito bem detalhada</p>
+            <h1>Ryan Lima Lessa</h1>
+            <p>Sou desenvolvedor, estudante da ETE Porto Digital. Entusiasta de jogos, rock'n roll e tecnologia, introvertido. Geralmente passo a maior parte do meu tempo no computador, conversando mais pela internet, saindo apenas para casos bem específicos, ir no cinema, praia, ou rolê com os amigos. </p>
             <button onClick={goToDescription}>Ver descrição</button>
         </div>
     );
